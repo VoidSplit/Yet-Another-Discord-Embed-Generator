33014ef0e466e740620d6134cf79dba986ee0185
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-07-31 23:50:01 +0200

    Add export

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import form from './form.js'
 import preview from './preview.js'
-import form_ui from './form_ui.js'
+import form_ui from './ui/form.js'
+import * as select from './ui/select.js'
+import * as templates from './export/templates.js'
 
 import { export_ } from './export.js'
 
-document.getElementById("test_button").addEventListener("click", (e) => {
-    const embed = form.getEmbed()
-    console.log(embed)
-    preview.preview(embed)
-    console.log(export_("python", embed))
+let syntaxes = {}
+Object.keys(templates).forEach((e) => {
+    syntaxes[e] = e
 })
+
+select.populate(syntaxes)
+
+window.exp = () => {
+    const embed = form.getEmbed()
+    const syntax = select.get()
+    const code = export_(syntax, embed)
+    document.getElementById("out").innerText = code
+}
 
 window.addField = form_ui.addField
