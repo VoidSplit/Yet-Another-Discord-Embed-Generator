f8aac11062ef4ec2407ef3eb6f13c6627ec3e7e3
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-08-02 00:17:48 +0200

    Refactor preview

--- src/js/preview.js
+++ src/js/preview.js
@@ -1,84 +1,14 @@
-function getElement(id, callback) {
-    const element = document.getElementById(`preview_${id}`)
-    if (element) {
-        callback(element)
-    } else {
-        return false
-    }
-}
-
-function putText(id, value) {
-    getElement(id, (element) => {
-        element.innerText = value
-    })
-}
-
-function setLinkUrl(id, value) {
-    getElement(id, (element) => {
-        if (value) {
-            element.href = value
-        } else {
-            element.removeAttribute("href")
-        }
-    })
-}
-
-function setImgSrc(id, value) {
-    getElement(id, (element) => {
-        element.src = value
-    })
-}
-
-function setLineColor(id, value) {
-    getElement(id, (element) => {
-        element.style.backgroundColor = value
-    })
-}
-
-function previewFields(fields) {
-    document.getElementById("fields").innerHTML = ""
-    if (fields.length > 0) {
-        let zone = null
-        fields.forEach((field) => {
-            if (!zone || !field.inline) {
-                zone = document.createElement("div")
-                zone.classList.add("field_zone")
-            }
-            
-            let field_div = document.createElement("div")
-            field_div.classList.add("field")
-
-            field_div.innerHTML = `
-                <div class="field_name">${field.name}</div>
-                <div class="field_value">${field.value}</div>
-            `
-
-            zone.appendChild(field_div)
-            document.getElementById("fields").appendChild(zone)
-
-            if (!field.inline) {
-                zone = null
-            }
-        })
-    }
-}
+import template from './preview.njs'
+import nunjucks from 'nunjucks'
 
 export function preview(embed) {
-    putText("title", embed.title || "Some title")
-    putText("description", embed.description || "Some description")
-    setLinkUrl("url", embed.url)
-    putText("timestamp", embed.timestamp || "Some timestamp")
-    setLineColor("color", embed.color || "#ffa600")
+    const env = nunjucks.configure({
+        autoescape: false,
+        trimBlocks: true,
+        lstripBlocks: true,
+    })
 
-    putText("footer_text", embed.footer.text || "Some footer text")
-    setImgSrc("footer_icon", embed.footer.icon_url || "resources/images/default profile picture.png")
+    console.log(embed)
 
-    setImgSrc("image", embed.image.url || "resources/images/default profile picture.png")
-
-    setImgSrc("thumbnail", embed.thumbnail.url || "resources/images/default profile picture.png")
-
-    putText("author_name", embed.author.name || "Some author name")
-    setImgSrc("author_icon", embed.author.icon_url || "resources/images/default profile picture.png")
-
-    previewFields(embed.fields)
+    document.getElementById("preview").innerHTML = env.renderString(template, embed)
 }
---
+++ src/js/preview.njs
@@ -0,0 +1,69 @@
+<div class="colored_line" id="preview_color"></div>
+<div class="inner">
+    {% if thumbnail.url %}
+        <div class="thumbnail">
+            <div class="wrapper">
+                <img src="{{ thumbnail.url }}" alt="" id="preview_thumbnail">
+            </div>
+        </div>
+    {% endif %}
+    <div class="main_content">
+        {% if author.name or author.icon_url %}
+        <div class="name_wrapper">
+            {% if author.icon_url %}
+            <div class="img">
+                <img src="{{ author.icon_url }}" alt="" srcset="" id="preview_author_icon">
+            </div>
+            {% endif %}
+            {% if author.name %}
+            <p class="name"><span id="preview_author_name">{{ author.name }}</span></p>
+            {% endif %}
+        </div>
+        {% endif %}é
+        {% if title %}
+        <div class="title">
+            <a {% if url %}href="{{ url }}"{% endif %} id="preview_url"><span id="preview_title">{{ title }}</span></a>
+        </div>
+        {% endif %}
+        {% if description %}
+        <div class="description">
+            <span id="preview_description">{{ description }}</span>
+        </div>
+        {% endif %}
+        {% if fields | length %}
+        <div id="fields">
+            {% for field in fields %}
+            <div class="field_zone">
+                <div class="field">
+                    <div class="field_name">{{ field.name }}</div>
+                    <div class="field_value">{{ field.value }}</div>
+                </div>
+            </div>
+            {% endfor %}
+        </div>
+        {% endif %}
+        {% if image.url %}
+        <div class="img_element">
+            <div class="wrapper">
+                <img src="{{ image.url }}" alt="" id="preview_image">
+            </div>
+        </div>
+        {% endif %}
+        {% if footer.text or footer.icon_url %}
+        <div class="footer">
+            {% if footer.icon_url %}
+            <div class="img">
+                <img src="{{ footer.icon_url }}" alt="" id="preview_footer_icon">
+            </div>
+            {% endif %}
+            {% if footer.text %}
+            <p class="text"><span id="preview_footer_text">{{ footer.text }}</span></p>
+            {% endif %}
+            <span class="decoration"></span>
+            {% if timestamp %}
+            <div class="timestamp"><span id="preview_timestamp">{{ timestamp }}</span></div>
+            {% endif %}
+        </div>
+        {% endif %}
+    </div>
+</div>
