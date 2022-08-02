4224868355d8052dc61387e7d9f71f4a1ed70d25
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-08-02 23:33:23 +0200

    Enforce limits

--- src/js/ui/form.js
+++ src/js/ui/form.js
@@ -1,23 +1,25 @@
 function addField() {
     const fields_element = document.getElementById("fields_wrapper")
 
-    let div = document.createElement("div")
+    if (fields_element.children.length < 25) {
+        let div = document.createElement("div")
 
-    div.id = "input_fields"
-    div.class = "field_line"
-    
-    div.innerHTML = `
-        <div class="field_line" id="input_fields">
-            <input type="text" placeholder="name" id="input_name">
-            <input type="text" placeholder="value" id="input_value">
-            <div class="checkbox">
-                <input type="checkbox" name="" id="input_inline">
-                <label>Inline</label>
+        div.id = "input_fields"
+        div.class = "field_line"
+        
+        div.innerHTML = `
+            <div class="field_line" id="input_fields">
+                <input type="text" placeholder="name" id="input_name" maxlength="256">
+                <input type="text" placeholder="value" id="input_value" maxlength="1024">
+                <div class="checkbox">
+                    <input type="checkbox" name="" id="input_inline">
+                    <label>Inline</label>
+                </div>
+                <button class="btn red" onclick="this.parentNode.parentNode.remove()">Delete</button>
             </div>
-            <button class="btn red" onclick="this.parentNode.parentNode.remove()">Delete</button>
-        </div>
-    `
-    fields_element.appendChild(div)
+        `
+        fields_element.appendChild(div)
+    }
 }
 
 window.addField = addField
