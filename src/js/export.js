a3b73333aaca2aa4efea7a098797fe0d4d9bca6a
Victor Bonnelle <victor.bonnelle@protonmail.com>
2022-08-02 12:10:20 +0200

    Improve export templates whitespace control

--- src/js/export.js
+++ src/js/export.js
@@ -21,6 +21,8 @@ export function export_(language, embed) {
         lstripBlocks: true,
     })
 
+    env.addFilter("section", (e) => { return e })
+
     env.addFilter("join_array", (arr, del1, del2) => {
         arr = arr.map(x => Object.values(x))
         arr = arr.map(x => x.join(del1))
